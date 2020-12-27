04b1cf8cb381ce9117891bdcb4561737585f0646
Hiru <hieuls369@gmail.com>
2020-12-27 14:49:58 +0700

    Add eat method

--- human.js
+++ human.js
@@ -3,6 +3,11 @@ function walk(){
 }
 
 function Human(){
-    this.height = 13;
-    this.weight = 45;
+    this.height = 13
+    this.weight = 45
+    this.stomach = []
+}
+
+Human.prototype.eat = function(food){
+    this.stomach.push(food)
 }
